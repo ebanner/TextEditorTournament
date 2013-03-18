223b59da0cfd09c17443c33933e4cb9b4b398150
Christopher Willig <willigcl193@potsdam.edu>
2013-03-18 13:49:42 -0400

    Added some test FTP server code to server.py. Not sure if it works.

--- server.py
+++ server.py
@@ -1,1 +1,15 @@
 #server from which we will receive the text file submissions (for judging/grading)
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+authorizer = DummyAuthorizer()
+authorizer.adduser("user", "12345", "/home/student/willigcl193", perm="elradfmw")
+#authorizer.add_anonymous("/home/nobody")
+
+handler = FTPHandler
+handler.authorizer = authorizer
+
+server = FTPServer(("127.0.0.1", 2100), handler)
+server.serve_forever()
