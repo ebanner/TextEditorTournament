779f0acc3fd55183dfc12439081cbdb8feff6d07
Christopher Willig <willigcl193@potsdam.edu>
2013-03-19 13:30:44 -0400

    The client can now send a text file to the server and the file is deposited in the users home directory (in this case, /home/student/willigcl193/Desktop).

--- client.py
+++ client.py
@@ -1,7 +1,10 @@
 #this is the client from which users will submit their text files
 
+import sys
 from ftplib import FTP
+
 ftp = FTP()
 ftp.connect('localhost', 2100)
 ftp.login('user', '12345')     # login
-ftp.retrlines('LIST')          # list directory contents
+#ftp.retrlines('LIST')          # list directory contents
+ftp.storlines("STOR " + str(sys.argv[1]), open(str(sys.argv[1])))
