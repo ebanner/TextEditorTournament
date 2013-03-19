2c719cbb9a7f4cbeb40d15a482be73cd96226733
Christopher Willig <willigcl193@potsdam.edu>
2013-03-19 13:23:58 -0400

    Last commit broke code, should not have pushed. Corrected mistake by making client unsecure.

--- client.py
+++ client.py
@@ -1,8 +1,7 @@
 #this is the client from which users will submit their text files
 
-from ftplib import FTP_TLS
-ftps = FTP_TLS()
-ftps.connect('localhost', 2100)
-ftps.login('user', '12345')     # login
-ftps.prot_p()                   # switch to secure data connection
-ftps.retrlines('LIST')          # list directory content securely
+from ftplib import FTP
+ftp = FTP()
+ftp.connect('localhost', 2100)
+ftp.login('user', '12345')     # login
+ftp.retrlines('LIST')          # list directory contents
