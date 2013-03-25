9d4ab492c760d9b29698ac0f6a3d38d96d382447
Edward Banner <edward.banner@gmail.com>
2013-03-25 04:01:38 -0400

    Editor now logged with participant.

--- server/server.py
+++ server/server.py
@@ -15,41 +15,40 @@ class SubmissionServer(socketserver.StreamRequestHandler):
     """
 
     def handle(self):
-        # Get the name of the participant
+        # Name of the participant
         self.participant = str(self.rfile.readline().strip(), 'utf-8')
         print('Participant: {}'.format(self.participant))
-        # Get the participant's editor
+        # Participant's editor
         self.editor = str(self.rfile.readline().strip(), 'utf-8')
         print('Editor: {}'.format(self.editor))
-        # Get the name of the submission file
+        # Name of the submission file
         self.file = str(self.rfile.readline().strip(), 'utf-8')
         print('Name of file: {}'.format(self.file))
-        # Get the number of bytes remaining in the file
+        # Number of bytes in the entire file
         self.length = int(self.rfile.readline().strip())
         print('Number of bytes: {}'.format(self.length))
 
         # Read in the entire body and convert it to a string
         self.body = self.rfile.read(self.length).decode('utf-8').split("\n")
         self.submission_lines = [ ''.join([line, "\n"]) for line in self.body ]
-        # Annoying non-existant line returned by split
         self.submission_lines.pop()
 
         # Open up the correct file and diff it with the submission
-        with open(self.file+'.sol', 'r') as f:
-            correct_lines = f.readlines()
+        with open(self.file+'.sol', 'r') as sol, open(self.file+'.log', 'a') as log: 
+            correct_lines = sol.readlines()
 
-            with open(self.file+'.log', 'a') as g:
-                # Log the participant
-                g.write(self.participant + "\n")
+            # Log the participant and his or her editor
+            log.write(self.participant + "\n")
+            log.write(self.editor + "\n")
 
-                for line in difflib.unified_diff(self.submission_lines, correct_lines,
-                        fromfile=self.file, tofile=self.file+'.sol'):
-                    self.wfile.write(bytes(line, 'utf-8'))
-                    # Log the participant's diff
-                    g.write(line)
+            for line in difflib.unified_diff(self.submission_lines, correct_lines,
+                    fromfile=self.file, tofile=self.file+'.sol'):
+                self.wfile.write(bytes(line, 'utf-8'))
+                # Log the participant's diff
+                log.write(line)
 
-                # Log the participant's time
-                g.write(str(time.time() - START) + "\n\n")
+            # Log the participant's time
+            log.write(str(time.time() - START) + "\n\n")
 
         # Print time elapsed
         end = time.time()
