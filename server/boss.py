e03467cc4c71282df0206e4b9704d0c7ce415d25
Richard Teammco <teammcr192@giant.(none)>
2013-04-05 20:50:29 -0400

    Connection closes and message sent to client when a client is done

--- server/boss.py
+++ server/boss.py
@@ -17,16 +17,11 @@ class Boss():
         print('Client added')
         
     def set_manager(self, client_sock):
-        print('in setmngr')
         mngr = manager.Manager(client_sock)
-        print('after constructor')
         if not hasattr(self, 'manager'):
-            print('before add')
             mngr.start()
-            print('before add')
             self.manager = mngr
             print('Manager added')
         else:
-            print('before reject')
             mngr.close()
-            print('manager not added')
+            print('Manager rejected (one already exists)')
