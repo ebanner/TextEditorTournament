d036e3f4283a5ea8bfe4693d32686ded97fc499d
teammcr192 <teammcr192@potsdam.edu>
2013-04-19 19:44:45 -0400

    Modified the html code for the Display system. It should now initialize the websocket and setup a connection to the server. There are also some function placeholders.

--- display/main.js
+++ display/main.js
@@ -3,18 +3,85 @@
  *  and displays them, along with audio playback.
  */
 
+
+// GLOBAL CONSTANTS
+var NOTIFICATION_TYPE_NORMAL = 0;
+var NOTIFICATION_TYPE_SYS = 1;
+var NOTIFICATION_TYPE_ALERT = 2;
+var NOTIFICATION_TYPE_ERROR = 3;
+
+
+// GLOBAL APP VARIABLES:
+var ws = false; // websocket (initially FALSE to imply it is not connected)
+
+
+// Initialize the websocket (try to connect to the WS server) using the given
+//  IP address and port number. If WebSockets are not supported, or it is
+//  already connected, returns FALSE. Otherwise, returns TRUE.
+function initWebSocket(ip, port){
+    // if websockets are not supported, report error and exit
+    if(!("WebSocket" in window)){
+        notify("WebSockets are not supported by this browser.",
+            NOTIFICATION_TYPE_ERROR);
+        return false;
+    }
+    
+    // if the websocket is already connected, report error and exit
+    if(ws){
+        alert("WebSocket already connected.",
+            NOTIFICATION_TYPE_ERROR);
+        return false;
+    }
+    
+    // Create a new websocket, and connect it to the server.
+    ws = new WebSocket("ws://" + ip + ":" + port + "/text_editing_tournament",
+        ['text_editing_tournament']);
+    
+    // When socket is open, send greetings message and notify connected.
+    ws.onopen = function(){
+        ws.send("Hello, server!");
+        notify("Connected to server.", NOTIFICATION_TYPE_SYS);
+    };
+    
+    // If socket experiences an error, display the error message.
+    ws.onerror = function(error) {
+        notify("The WebSocket has experienced an error.",
+            NOTIFICATION_TYPE_ERROR);
+        //console.log('WebSocket Error ' + error); TODO - what?
+    };
+    
+    // If socket is closed, display notification alert, and set ws to false
+    //  to indicate that it is no longer connected.
+    ws.onclose = function(){
+        notify("Disconnected from server.", NOTIFICATION_TYPE_ALERT);
+        ws = false;
+    }
+    
+    // Parse any messages that the server sends.
+    ws.onmessage = function(e){
+        parseServerMessage(e.data);
+    };
+    
+    return true;
+}
+
+
 // When document is loaded, initialize the websocket, add a listener to the
 //  input box, and zero-out all of the text values that may have been cached
 //  from before.
 $(document).ready(function(){
-    // check if audio is supported in this browser
+    // Check if audio is supported in this browser. If not, alert the message.
     var audioTagSupport = !!(document.createElement('audio').canPlayType);
-    if (!audioTagSupport)
-        alert("Can't play audio.");
+    if (!audioTagSupport){
+        notify("Audio playback is not supported by this browser.",
+            NOTIFICATION_TYPE_ALERT);
+    }
         
-    // TODO - init websockets here
+    // Try to initialize the websocket here.
+    var retval = initWebSocket("127.0.0.1", 9999);
+    alert(retval);
     
-    // set up inputBox to call sendMessage function
+    // Set up inputBox to call sendMessage function.
     $("#inputBox").keyup(function(e){
         if(e.keyCode == 13) // enter
             //sendMessage(); TODO - change this back
@@ -25,7 +92,25 @@ $(document).ready(function(){
     $("#inputBox").val("");
     $("#textBox").val("");
 });
-    
+
+
+// Parse a message received from the server through the websocket.
+function parseServerMessage(data){
+    alert(data); // TODO - just alerts for now
+}
+
+// Display a notification bubble on the screen that will show the given
+//  text for a while, and then fade out.
+// PARAMETERS:  text: the text to be displayed
+//              type: depending on the given type, the color of the text/bubble
+//                  will varry.
+function notify(text, type){
+    alert(text); // TODO - just alerts for now
+}
+
+
+// Queries Google Translate text-to-speech API to playback the given text.
+//  NOTE: currently, this does not work on Firefox.    
 function playText(text){
     if (text == "")
         return false;
