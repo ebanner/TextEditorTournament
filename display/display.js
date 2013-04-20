ae6ae8dd7e38f6bef2b9f08f92d2b7481ece7d1f
teammcr192 <teammcr192@potsdam.edu>
2013-04-20 00:36:16 -0400

    Challenge mode Display code now separates colums well and scales with resizing the canvas. Also, there is now a main header row that provides a key for what each of the columns are.

--- display/display.js
+++ display/display.js
@@ -71,15 +71,38 @@ function DisplayChallengeMode(){
 		var numCompetitors = this.competitors.length;
 		if(numCompetitors >= 1){
 		    // compute box height: at least 1/15th of the canvas height
-		    var boxHeight = Math.floor(this.canvas.height / numCompetitors);
+		    var boxHeight = Math.floor(this.canvas.height / (numCompetitors+1));
 		    boxHeight = Math.min(boxHeight, this.canvas.height/15);
 		    
 		    // compute line positions
 		    var curY = 0;
-		    var firstDivider = midpointX + 35;
-		    var secondDivider = midpointX + 150;
-		    var thirdDivider = midpointX + 450;
+		    var firstDivider = midpointX + Math.floor(rightWidth / 6.5);
+		    var secondDivider = midpointX + Math.floor(rightWidth / 1.75);
+		    var boxTextSize = Math.ceil(boxHeight/2.6);
+		    var relativeTextY = Math.ceil((boxHeight/2) + (boxTextSize/3));
+		    var textOffset = Math.ceil(rightWidth/60);
+		    var firstTextX = midpointX + textOffset;
+		    var secondTextX = firstDivider + textOffset;
+		    var thirdTextX = secondDivider + textOffset;
 		    this.ctx.lineWidth = Math.ceil(lineWidth / 2);
+		    
+		    // first box is the title
+		    this.ctx.fillStyle = "#FFFFFF";
+		    this.ctx.fillRect(midpointX, curY, rightWidth, boxHeight);
+		    this.ctx.beginPath();
+		        this.ctx.moveTo(midpointX, curY + boxHeight);
+		        this.ctx.lineTo(this.canvas.width, curY + boxHeight);
+		        this.ctx.stroke();
+		    this.ctx.closePath();
+		    this.ctx.fillStyle = "#000000";
+		    this.ctx.font = "bold " + boxTextSize + "pt Arial";
+		    this.ctx.fillText("Status", firstTextX, relativeTextY);
+		    this.ctx.fillText("Participant", secondTextX, relativeTextY);
+		    this.ctx.fillText("Editor", thirdTextX, relativeTextY);
+		    
+		    // set for other sections
+		    this.ctx.font = "regular " + boxTextSize + "pt Arial";
+		    curY += boxHeight;
 		    
 		    for(var i=0; i<numCompetitors; i++){
 		    
@@ -87,13 +110,13 @@ function DisplayChallengeMode(){
 		        var bgColor = "#FFFFFF";
 		        switch(this.competitors[i].status){
 		            case PARTICIPANT_WORKING:
-		                bgColor = "#AAAAAA";
+		                bgColor = "#CCCCCC";
 		                break;
 		            case PARTICIPANT_FINISHED:
-		                bgColor = "#00FF00";
+		                bgColor = "#AAFFAA";
 		                break;
 		            case PARTICIPANT_FORFEIT:
-		                bgColor = "#FF0000";
+		                bgColor = "#FF8888";
 		                break;
 		            default:
 		                break;
@@ -112,7 +135,7 @@ function DisplayChallengeMode(){
 		        curY += boxHeight;
 		    }
 		        
-		    // draw first column divider (placement|name)
+		    // draw first column divider (status|name)
 		    this.ctx.beginPath();
 		        this.ctx.moveTo(firstDivider, 0);
 		        this.ctx.lineTo(firstDivider, curY);
@@ -123,13 +146,6 @@ function DisplayChallengeMode(){
 		    this.ctx.beginPath();
 		        this.ctx.moveTo(secondDivider, 0);
 		        this.ctx.lineTo(secondDivider, curY);
-		        this.ctx.stroke();
-		    this.ctx.closePath();
-		    
-		    // draw third column divider (editor|animation)
-		    this.ctx.beginPath();
-		        this.ctx.moveTo(thirdDivider, 0);
-		        this.ctx.lineTo(thirdDivider, curY);
 		        this.ctx.stroke();
 		    this.ctx.closePath();
 		}
